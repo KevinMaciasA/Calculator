79219fae711e3ccedc9fbf82d266400d3e3ad5bf
Kevin <kevin.macias.a@gmail.com>
2022-05-03 17:48:00 -0700

    Feature: calculate triangle perimeter & area

--- index.js
+++ index.js
@@ -2,25 +2,50 @@ function Square(sideSize = 1) {
   this.sideSize = sideSize;
 
   this.perimeter = () => {
-    return this.sideSize * 4 + " u";
+    return this.sideSize * 4;
   };
 
   this.area = () => {
-    return this.sideSize * this.sideSize + " u^2";
+    return this.sideSize * this.sideSize;
   };
 }
 
+function Triangle(sideOne = 1, sideTwo = 2, base = 3, height = 4) {
+  this.sideOne = sideOne;
+  this.sideTwo = sideTwo;
+  this.base = base;
+  this.height = height;
+
+  this.perimeter = () => this.sideOne + this.sideTwo + this.base;
+  this.area = () => (this.base * this.height) / 2;
+}
+
 const square = new Square();
+const tringle = new Triangle();
 
 function calculateSquarePerimeter() {
-  //getInput
-  const input = document.getElementById("squareSize").value;
+  const input = parseInt(document.getElementById("square-size").value);
   square.sideSize = input;
-  alert(square.perimeter());
+  alert(square.perimeter() + " u");
 }
 function calculateSquareArea() {
-  //getInput
-  const input = document.getElementById("squareSize").value;
+  const input = parseInt(document.getElementById("square-size").value);
   square.sideSize = input;
-  alert(square.area());
+  alert(square.area() + " u^2");
+}
+function calculateTrianglePerimeter() {
+  const base = parseInt(document.getElementById("triangle-base").value);
+  const sizeOne = parseInt(document.getElementById("triangle-size-1").value);
+  const sizeTwo = parseInt(document.getElementById("triangle-size-2").value);
+  tringle.base = base;
+  tringle.sideOne = sizeOne;
+  tringle.sideTwo = sizeTwo;
+  alert(tringle.perimeter() + " u");
+}
+function calculateTriangleArea() {
+  const base = parseInt(document.getElementById("triangle-base").value);
+  const height = document.getElementById("triangle-height").value;
+  tringle.base = base;
+  tringle.height = height;
+  alert(tringle.area() + " u^2");
 }
